eb8b3392f898840575ffeaf5946aa8b51adb39f1
Georges Briche <gbriche59@yahoo.fr>
2023-05-08 12:08:20 +0200

    Finalisation Slide

--- src/components/Slide/index.jsx
+++ src/components/Slide/index.jsx
@@ -1,15 +1,26 @@
-import '../../styles/Slide.css';
+import vectorBas from '../../assets/VectorBas.png'
+import vectorHaut from '../../assets/VectorHaut.png'
 
 function Slide(props) {
+
+  function ToggleText(visible, setVisible){
+    setVisible(!visible)
+  };
 
   return (
     <div className='slide'>
             <h2 className='slide-titre'>
               {props.titre}
-              <button onClick={ToggleText}><img className='slide-icone' alt="slide-icone" src={vectorBas}></img></button>
+              <button onClick={() => ToggleText(props.visible, props.setVisible)}>
+                {!props.visible ?
+                  <img className='slide-icone' alt="slide-icone" src={vectorBas}></img>
+                  :
+                  <img className='slide-icone' alt="slide-icone" src={vectorHaut}></img>
+                }
+              </button>
             </h2>
             {props.visible && <p className='slide-text'>
-              {props.titre}
+              {props.text}
             </p>}
           </div>
   );
--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import '../../styles/Apropos.css';
 import Banner from '../../components/Banner'
 import bannerPhoto from '../../assets/kalen-emsley-montagnes foret.webp'
-import vectorBas from '../../assets/VectorBas.png'
-import vectorHaut from '../../assets/VectorHaut.png'
+import Slide from '../../components/Slide'
 
 
 function Apropos() {
@@ -12,67 +11,43 @@ function Apropos() {
   const [visibleRespect, setVisibleRespect] = useState(false);
   const [visibleService, setVisibleService] = useState(false);
   const [visibleSecurite, setVisibleSecurite] = useState(false);
-
-  function ToggleText(event, visible, setVisible){
-    setVisible(!visible)
-    const element = event.target
-  
-    if (element.src===vectorBas){
-      element.src=vectorHaut
-    }
-    else{
-      element.src=vectorBas
-    } 
-  };
   
   return (
     <div>
         <Banner img={bannerPhoto} text='Chez vous, partout et ailleurs'/>
         <div className='apropos-global'>
-          <div className='apropos'>
-            <h2 className='apropos-titre'>
-              Fiabilité
-              <button onClick={(event) => ToggleText(event, visibleFiabilite, setVisibleFiabilite)}><img className='apropos-icone' alt="apropos-icone" src={vectorBas}></img></button>
-            </h2>
-            {visibleFiabilite && <p className='apropos-text'>
-              Les annonces postées sur Kasa garantissent une fiabilité totale. 
+          <Slide 
+            titre="Fiabilite" 
+            text="Les annonces postées sur Kasa garantissent une fiabilité totale. 
               Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  
-              par nos équipes.
-            </p>}
-          </div>
-          <div className='apropos'>
-            <h2 className='apropos-titre'>
-              Respect
-              <button onClick={(event) => ToggleText(event, visibleRespect, setVisibleRespect)}><img className='apropos-icone' alt="apropos-icone" src={vectorBas}></img></button>
-            </h2>
-            {visibleRespect &&<p className='apropos-text'>
-              La bienveillance fait partie des valeurs fondatrices de Kasa. 
-              Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
-            </p>}
-          </div>
-          <div className='apropos'>
-            <h2 className='apropos-titre'>
-              Service
-              <button onClick={(event) => ToggleText(event, visibleService, setVisibleService)}><img className='apropos-icone' alt="apropos-icone" src={vectorBas}></img></button>
-            </h2>
-            {visibleService &&<p className='apropos-text'>
-              Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. 
-              N'hésitez pas à nous contacter si vous avez la moindre question.
-            </p>}
-          </div>
-          <div className='apropos'>
-            <h2 className='apropos-titre'>
-              Sécurité
-              <button onClick={(event) => ToggleText(event, visibleSecurite, setVisibleSecurite)}><img className='apropos-icone' alt="apropos-icone" src={vectorBas}></img></button>
-            </h2>
-            {visibleSecurite &&<p className='apropos-text'>
-              La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
+              par nos équipes."
+            visible = {visibleFiabilite}
+            setVisible = {setVisibleFiabilite}
+          />
+          <Slide 
+            titre="Respect" 
+            text="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+            Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+            visible = {visibleRespect}
+            setVisible = {setVisibleRespect}
+          />
+          <Slide 
+            titre="Service" 
+            text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. 
+              N'hésitez pas à nous contacter si vous avez la moindre question."
+            visible = {visibleService}
+            setVisible = {setVisibleService}
+          />
+          <Slide 
+            titre="Sécurité" 
+            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
               chaque logement correspond aux critères de sécurité établis par nos services. 
               En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier 
               que les standards sont bien respectés. Nous organisons également des ateliers sur 
-              la sécurité domestique pour nos hôtes.
-            </p>}
-          </div>
+              la sécurité domestique pour nos hôtes."
+            visible = {visibleSecurite}
+            setVisible = {setVisibleSecurite}
+          />
         </div>
     </div> 
   );
