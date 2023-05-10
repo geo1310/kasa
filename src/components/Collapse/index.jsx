11c99adfd511ce93b852b2bd212c8a7c9eb42cbf
Georges Briche <gbriche59@yahoo.fr>
2023-05-10 10:19:20 +0200

    Mise a jour Code

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,7 +1,6 @@
 import vectorBas from '../../assets/icones/VectorBas.png'
 import vectorHaut from '../../assets/icones/VectorHaut.png'
 
-<<<<<<< HEAD:src/components/Collapse/index.jsx
 function Collapse(props) {
 
   function ToggleText(visible, setVisible){
@@ -28,38 +27,3 @@ function Collapse(props) {
 }
 
 export default Collapse;
-=======
-function Slide(props) {
-    function ToggleText(visible, setVisible) {
-        setVisible(!visible)
-    }
-
-    return (
-        <div className="slide">
-            <h2 className="slide-titre">
-                {props.titre}
-                <button
-                    onClick={() => ToggleText(props.visible, props.setVisible)}
-                >
-                    {!props.visible ? (
-                        <img
-                            className="slide-icone"
-                            alt="slide-icone"
-                            src={vectorBas}
-                        ></img>
-                    ) : (
-                        <img
-                            className="slide-icone"
-                            alt="slide-icone"
-                            src={vectorHaut}
-                        ></img>
-                    )}
-                </button>
-            </h2>
-            {props.visible && <p className="slide-text">{props.text}</p>}
-        </div>
-    )
-}
-
-export default Slide
->>>>>>> 8606e72c5202bff25acea6e5f124ae3ae5bfd521:src/components/Slide/index.jsx
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,6 @@ import logo from '../../assets/logos/logo-kasa.svg'
 import '../../styles/Header.css'
 
 function Header() {
-<<<<<<< HEAD
   return (
     <header className="header">
 		<img src={logo} alt='logo kasa header' className='logo-kasa-header'></img>
@@ -15,31 +14,6 @@ function Header() {
 		</div>
 	</header>
   );
-=======
-    return (
-        <header className="header">
-            <img
-                src={logo}
-                alt="logo kasa header"
-                className="logo-kasa-header"
-            ></img>
-            <div className="nav">
-                <ul>
-                    <li>
-                        <Link className="nav-accueil" to="/">
-                            Accueil
-                        </Link>
-                    </li>
-                    <li>
-                        <Link className="nav-apropos" to="/apropos">
-                            A propos
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    )
->>>>>>> 8606e72c5202bff25acea6e5f124ae3ae5bfd521
 }
 
 export default Header
