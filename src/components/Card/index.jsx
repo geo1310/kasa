f9f23969bb0d9d13eb865d7504b4ff76887f410e
Georges Briche <gbriche59@yahoo.fr>
2023-05-10 15:43:17 +0200

    Mise a jour Code

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,12 @@
+import { HousingList } from '../../datas/HousingList'
+import '../../styles/Card.css'
+
+function Card(props) {
+    return (
+        <div className='card'>
+
+        </div>
+    )
+}
+
+export default Card
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,29 @@
+import Card from '../../components/Card'
 import '../../styles/Home.css'
+
 import Banner from '../../components/Banner'
 import bannerPhoto from '../../assets/images/mer cote.webp'
 
 function Home() {
-    document.querySelector('.nav-accueil') &&
-        document.querySelector('.nav-accueil').classList.add('active')
+document.querySelector('.nav-accueil') &&
+    document.querySelector('.nav-accueil').classList.add('active')
     document.querySelector('.nav-apropos') &&
-        document.querySelector('.nav-apropos').classList.remove('active')
+    document.querySelector('.nav-apropos').classList.remove('active')
 
     return (
         <div>
             <Banner img={bannerPhoto} text="Chez vous, partout et ailleurs" />
-            <div className="accueil">Page d'Accueil</div>
+            <div className="cards">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
