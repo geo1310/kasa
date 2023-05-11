f45aa20c87f32a44f61d8fe683fe6072fe6360ec
Georges Briche <gbriche59@yahoo.fr>
2023-05-11 22:41:44 +0200

    Mise a jour Code

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -21,33 +21,25 @@ function Apropos() {
             <div className="apropos-global">
                 <Collapse
                     titre="Fiabilite"
-                    text="Les annonces postées sur Kasa garantissent une fiabilité totale. 
-              Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  
-              par nos équipes."
+                    listeTexte={["Les annonces postées sur Kasa garantissent une fiabilité totale.Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."]}
                     visible={visibleFiabilite}
                     setVisible={setVisibleFiabilite}
                 />
                 <Collapse
                     titre="Respect"
-                    text="La bienveillance fait partie des valeurs fondatrices de Kasa. 
-            Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+                    listeTexte={["La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."]}
                     visible={visibleRespect}
                     setVisible={setVisibleRespect}
                 />
                 <Collapse
                     titre="Service"
-                    text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. 
-              N'hésitez pas à nous contacter si vous avez la moindre question."
+                    listeTexte={["Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.N'hésitez pas à nous contacter si vous avez la moindre question."]}
                     visible={visibleService}
                     setVisible={setVisibleService}
                 />
                 <Collapse
                     titre="Sécurité"
-                    text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
-              chaque logement correspond aux critères de sécurité établis par nos services. 
-              En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier 
-              que les standards sont bien respectés. Nous organisons également des ateliers sur 
-              la sécurité domestique pour nos hôtes."
+                    listeTexte={["La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."]}
                     visible={visibleSecurite}
                     setVisible={setVisibleSecurite}
                 />
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,27 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { housingList } from '../../datas/housingList'
 import Carousel from '../../components/Carousel'
 import Collapse from '../../components/Collapse'
+import Error from '../../components/Error'
 import Tags from '../../components/Tags'
 import '../../styles/Logement.css'
 
-function Logement(props) {
-
+function Logement() {
     const [visibleDescription, setVisibleDescription] = useState(true)
     const [visibleEquipements, setVisibleEquipements] = useState(true)
-
+    const [isId, setIsId] = useState(false)
+    const [houseIndex, setHouseIndex]= useState()
     document.querySelector('.nav-accueil') &&
-        document.querySelector('.nav-accueil').classList.remove('active')
+    document.querySelector('.nav-accueil').classList.remove('active')
     document.querySelector('.nav-apropos') &&
-        document.querySelector('.nav-apropos').classList.remove('active')
-
+    document.querySelector('.nav-apropos').classList.remove('active')
     const { id } = useParams()
 
+    useEffect(() => {
+        housingList.forEach((house) =>{
+            if(house.id===id){
+                setIsId(true)
+                setHouseIndex(housingList.indexOf(house))
+                const houseChoice = housingList[houseIndex] 
+            }
+        })
+    },  )
+
     return (
-        <div className="house-form">
+        <div className="house">
             {housingList.map((house) => {
-                if (house.id === id) {
+                if (house.id === id) {             
                     return (
                         <div key={house.id} className="house-global">
                             <Carousel pictures={house.pictures} />
@@ -44,22 +54,21 @@ function Logement(props) {
                                 </div>
                             </div>
                             <div className="house-tags">
-                                <Tags tags={house.tags}/>
+                                <Tags tags={house.tags} />
                             </div>
                             <div className="house-collapse">
-                                <Collapse 
-                                    titre='Description' 
-                                    text={house.description}
+                                <Collapse
+                                    titre="Description"
+                                    listeTexte={house.description.split()}
                                     visible={visibleDescription}
                                     setVisible={setVisibleDescription}
                                 />
-                                <Collapse 
-                                    titre='Equipements' 
-                                    text={house.equipments}
+                                <Collapse
+                                    titre="Equipements"
+                                    listeTexte={house.equipments}
                                     visible={visibleEquipements}
                                     setVisible={setVisibleEquipements}
                                 />
-                                
                             </div>
                         </div>
                     )
@@ -68,6 +77,7 @@ function Logement(props) {
             })}
         </div>
     )
+    
 }
 
 export default Logement
